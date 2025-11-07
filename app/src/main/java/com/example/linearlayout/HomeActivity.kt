de7d8d7f6f63f60305d87c475f73243b49368fff
Lucas Lima Rodrigues <lucaslrodri@gmail.com>
2025-11-06 19:56:34 -0500

    Intents + Bundle + Navegação entre telas...

---
+++ app/src/main/java/com/example/linearlayout/HomeActivity.kt
@@ -0,0 +1,36 @@
+package com.example.linearlayout
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.linearlayout.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.main)
+
+        val emailText = intent.getStringExtra("EMAIL_KEY")
+
+        // val bundle = intent.extras
+        if (emailText != null) {
+           // Usar valores recebidos
+            binding.textEmail.text = emailText
+        } else {
+            Toast.makeText(this, "Erro ao receber dados", Toast.LENGTH_SHORT).show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/linearlayout/LoginActivity.kt
+++ app/src/main/java/com/example/linearlayout/LoginActivity.kt
@@ -1,27 +1,76 @@
 package com.example.linearlayout
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.EditText
+import android.widget.Toast
+import com.example.linearlayout.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Inicializar o binding
+        // Inflate: Criar a view a partir do Layout
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        // Carrego meu XML
-        setContentView(R.layout.activity_login)
+        // Carrego minha interface a partir do objeto binding
+        setContentView(binding.main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left + binding.main.paddingStart,
+                systemBars.top,
+                systemBars.right + binding.main.paddingEnd,
+                systemBars.bottom
+            )
             insets
         }
+        // Usar camel case
+        // snake_case - separo_as_palavras_com_underscore
+        // camelCase - cadaPalavraComecaComMaiuscula
+        binding.buttonLogin.setOnClickListener(this)
+    }
 
-        val email = findViewById<EditText>(R.id.edittext_email)
-        val email_text = email.text.toString()
-        val password = findViewById<EditText>(R.id.edittext_password)
+    override fun onClick(view: View){
+        when(view.id){
+            binding.buttonLogin.id -> { // ou R.id.buttonLogin
+                // Validação de campos vazios
+                val emailText: String = binding.edittextEmail.text.toString()
+                val passwordText: String = binding.edittextPassword.text.toString()
 
+                if (emailText.isNotEmpty() && passwordText.isNotEmpty()) {
+                    // Prossegue para fazer o processo de login
+                    // ::class.java -> Referencia
+                    val intent = Intent(this, HomeActivity::class.java)
+                    val bundle = Bundle()
+                    bundle.putString("EMAIL_KEY", emailText)
+//                    bundle.putString("PASSWORD_KEY", passwordText)
+                    intent.putExtras(bundle)
+                    //intent.putExtra("EMAIL_KEY", emailText)
+                    //intent.putExtra("PASSWORD_KEY", passwordText)
+                    startActivity(intent)
+                } else {
+                    // Mandar um alerta...
+                    // Primeiro parâmetro -> Contexto:
+                    // - Recursos
+                    // - Estados
+                    // - Funcionamento
+                    // Segundo parâmetro -> Mensagem
+                    // Terceiro parâmetro -> Duração
+                    // - Short
+                    // - Long
+                    Toast.makeText(this, R.string.login_empty_field, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
